0d96bfeae96a05b725fabfcb62ac1add554b3ffc
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-11 00:10:49 +0100

    Add function addImageWatermarkToImage

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
 };
 
 addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world');
+
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+
+  image.composite(watermark, 0, 0, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+  await image.quality(100).writeAsync(outputFile);
+};
+
+addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
